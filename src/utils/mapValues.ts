ef9f727944d7e8d716e92f2c4b8c35d1eb356088
Guten Ye <377544+gutenye@users.noreply.github.com>
2025-12-31 22:05:52 +0800

    add prepublishOnly script

--- src/utils/mapValues.ts
+++ src/utils/mapValues.ts
@@ -1,8 +1,8 @@
 export function mapValues<T extends Record<string, any>, R>(
   obj: T,
-  fn: (value: T[keyof T]) => R
+  fn: (value: T[keyof T]) => R,
 ): Record<keyof T, R> {
   return Object.fromEntries(
-    Object.entries(obj).map(([k, v]) => [k, fn(v)])
-  ) as Record<keyof T, R>;
+    Object.entries(obj).map(([k, v]) => [k, fn(v)]),
+  ) as Record<keyof T, R>
 }
